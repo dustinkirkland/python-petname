5dd92ee515bf49f494c8d63fe0b5ef9e4c61c70d
Dustin Kirkland <dustin.kirkland@gmail.com>
2014-12-15 22:33:09 -0600

    * adjectives.txt, adverbs.txt, debian/control, debian/rules,   names.txt, petname/__init__.py, update.sh => debian/update-   wordlists.sh:   - update the word list, build-depend on petname package which     provides the word list

--- debian/update-wordlists.sh
+++ debian/update-wordlists.sh
@@ -6,10 +6,10 @@ PKG="petname"
 
 for f in adverbs adjectives names; do
 	rm -f "$f".txt.list
-	for w in $(cat "$f".txt); do
+	for w in $(cat /usr/lib/petname/"$f".txt); do
 		printf '"%s", ' "$w" >> "$f".txt.list
 	done
 	sed -i -e "s/, $//" "$f".txt.list
-	sed -i -e "s/^$f = [.*$/$f = [$(cat ${f}.list)]/" ${PKG}/${PKG}.py
+	sed -i -e "s/^$f = .*$/$f = \[$(cat ${f}.txt.list)\]/" ${PKG}/__init__.py
 	rm -f "$f".txt.list
 done
