6351b9798ecc436a7159817dca60948719cdd2a2
Dustin Kirkland <dustin.kirkland@gmail.com>
2015-01-12 14:36:26 -0600

    opening 1.4

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
 		name='petname',
 		description='Generate human-readable, random object names',
 		long_description=readme,
-		version='1.3',
+		version='1.4',
 		author='Dustin Kirkland',
 		author_email='dustin.kirkland@gmail.com',
 		license="Apache2",
