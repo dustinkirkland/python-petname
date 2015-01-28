0a53173a9861a7387dcc4b7ffc7178d20a3a1873
Dustin Kirkland <dustin.kirkland@gmail.com>
2015-01-28 15:43:04 -0600

    * debian/update-wordlists.sh, petname/__init__.py, README.md:   - rebuild with new petname wordlists

--- debian/update-wordlists.sh
+++ debian/update-wordlists.sh
@@ -6,11 +6,15 @@ PKG="petname"
 
 for f in adverbs adjectives names; do
 	rm -f "$f".txt.list
+	printf "\n$f = [" > "$f".txt.list
 	for w in $(cat /usr/share/petname/"$f".txt); do
 		printf '"%s", ' "$w" >> "$f".txt.list
 	done
-	sed -i -e "s/, $//" "$f".txt.list
-	sed -i -e "s/^$f = .*$/$f = \[$(cat ${f}.txt.list)\]/" ${PKG}/__init__.py
-	rm -f "$f".txt.list
+	sed -i -e "s/, $/]\n\n/" "$f".txt.list
+	sed -i "/^$f = .*$/d" ${PKG}/__init__.py
 done
+grep -B 1000 "^import " ${PKG}/__init__.py > above
+grep -A 1000 "^def Adverb" ${PKG}/__init__.py > below
+cat above *.txt.list below > ${PKG}/__init__.py
+rm -f *.txt.list above below
 cat /usr/share/doc/petname/README.md > README.md
