320e9db93899800125cf1092e82e55cb574a62a1
Dustin Kirkland <kirkland@ubuntu.com>
2016-02-16 11:55:04 -0800

    * setup.py   - fix typo in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with ssh-import-id.  If not, see <http://www.gnu.org/licenses/>.
+# along with python-petname.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 from setuptools import setup
