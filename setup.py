ed2a54a032ed1095aaf6bf4e818bab25c76c4895
Dustin Kirkland <kirkland@ubuntu.com>
2016-08-09 14:21:33 -0500

    * petname/english.py, README.md, setup.py:   - bump to 2.0, using the new petname 2.0 wordlists, which are smaller,     simpler, and use animal names instead of people names

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     name='petname',
     description='Generate human-readable, random object names',
     long_description=readme,
-    version='1.13',
+    version='2.0',
     author='Dustin Kirkland',
     author_email='dustin.kirkland@gmail.com',
     license="Apache2",
