32e516ab678ce4c75de306ad7e9db7b83ba6f293
Dustin Kirkland <kirkland@ubuntu.com>
2017-04-27 17:24:00 -0500

    opening 2.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
 	name='petname',
 	description='Generate human-readable, random object names',
 	long_description=readme,
-	version='2.1',
+	version='2.2',
 	author='Dustin Kirkland',
 	author_email='dustin.kirkland@gmail.com',
 	license="Apache2",
