bef1ec9bf171779cfc8b6ceb104a21b18531e9ec
Dustin Kirkland <dustin.kirkland@gmail.com>
2024-04-22 10:45:45 -0500

    use github as authoritative in the update maintainer script remove a couple of animals

--- debian/update-wordlists.sh
+++ debian/update-wordlists.sh
@@ -10,7 +10,7 @@ PKG="petname"
 for f in adverbs adjectives names; do
 	rm -f "$f".txt.list
 	printf "\n$f = [" > "$f".txt.list
-	for w in $(cat /usr/share/petname/"$f".txt); do
+	for w in $(wget -q -O- https://raw.githubusercontent.com/dustinkirkland/petname/master/usr/share/petname/small/${f}.txt); do
 		printf '"%s", ' "$w" >> "$f".txt.list
 	done
 	sed -i -e "s/, $/]\n\n/" "$f".txt.list
@@ -19,4 +19,4 @@ done
 sed -i -e "/^adjectives = /d" -e "/^adverbs = /d" -e "/^names = /d" -e "/^$/d" ${PKG}/english.py
 cat *.txt.list >> ${PKG}/english.py
 rm -f *.txt.list
-cat /usr/share/doc/petname/README.md > README.md
+wget -q -O- https://raw.githubusercontent.com/dustinkirkland/petname/master/README.md > README.md
